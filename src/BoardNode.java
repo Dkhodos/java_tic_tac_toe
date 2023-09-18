2cbc3e80c59282eb768b2b89aac99c82d749d270
Daniel Khodos <dkhodos94@gmail.com>
2023-09-18 14:07:23 +0000

    first

---
+++ src/BoardNode.java
@@ -0,0 +1,7 @@
+public class BoardNode() {
+    private BoardState state = BoardState.EMPTY;
+
+    public setState(BoardState state){
+        this.state = state;
+    }
+}
---
+++ src/Settings.java
@@ -0,0 +1,8 @@
+public class Settings {
+
+    // enable / disable debug logs in program
+    public static boolean DEBUG = false;
+
+    // board size for a nXn game board
+    public static int BOARD_SIZE = 4;
+}
